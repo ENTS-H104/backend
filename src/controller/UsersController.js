caee2701a8c99912d87ee546fcea0f5e0e38ccfe
David <daviddwiyanto79@gmail.com>
2024-05-25 17:34:01 +0700

    feat(JWT, User Logout)

--- src/controller/UsersController.js
+++ src/controller/UsersController.js
@@ -10,6 +10,8 @@ const {
     sendPasswordResetEmail,
     } = require('firebase/auth');
 
+const jwt = require('jsonwebtoken');
+const blacklist = new Set();
 
 const getAllUsers = async (req, res) => {
     try {
@@ -78,7 +80,10 @@ const registerUsers = async (req, res) => {
         const uid = userCredential.user.uid;
         const uuid = uuidv4();
 
-        await UsersModel.registerUsers(uid, email, phone_number, username, uuid);
+        const [ defaultRole ] = await UsersModel.getDefaultUserRole();
+        const defaultRoleToString = JSON.stringify(defaultRole[0].user_role_uuid)
+        
+        await UsersModel.registerUsers(uid, email, phone_number, username, uuid, defaultRoleToString);
         res.status(201).json({
             status: 201,
             message: `Successfully created an account, Verification email sent. Please check your email`,
@@ -131,60 +136,22 @@ const loginUsers = async (req, res) => {
         
         // Check if email is verified
         if (userCredential.user.emailVerified) {
-            const [ data ] = await UsersModel.getCurrentUser(auth.currentUser.uid);
-            // Convert UTC timestamps to UTC+7
-            const dataWithLocalTime = data.map(data => ({
-                ...data,
-                created_at: moment.utc(data.created_at).tz('Asia/Bangkok').format(),
-                updated_at: moment.utc(data.updated_at).tz('Asia/Bangkok').format()
-            }));
+            const token = jwt.sign({ uid: auth.currentUser.uid }, process.env.JWT_SECRET_KEY, { expiresIn: '60d' });
+
             res.status(201).json({
                 status: 201,
-                message: `Successfully login an user with uid: ${auth.currentUser.uid}`,
-                data: dataWithLocalTime
+                message: `User with email: ${email} Successfully login`,
+                token: token
             });
+            await auth.signOut();
         } else {
             // If email is not verified, return 403
-            await auth.signOut();
             res.status(403).json({
                 status: 403,
                 code: "not-verified",
                 message: 'Email not verified. Please verify your email before logging in.'
             });
-        }
-        
-    } catch (error) {
-        res.status(500).json({
-            status: 500,
-            message: "Server Error",
-            serverMessage: error,
-        })
-    }
-}
-
-const currentUsers = async (req, res) => {
-    try {
-        // Check if email is verified
-        if (auth.currentUser) {  
-            const [ data ] = await UsersModel.getCurrentUser(auth.currentUser.uid);
-            // Convert UTC timestamps to UTC+7
-            const dataWithLocalTime = data.map(data => ({
-                ...data,
-                created_at: moment.utc(data.created_at).tz('Asia/Bangkok').format(),
-                updated_at: moment.utc(data.updated_at).tz('Asia/Bangkok').format()
-            }));
-            res.status(200).json({
-                status: 200,
-                message: `Successfully fetched an user with uid: ${auth.currentUser.uid}`,
-                data: dataWithLocalTime
-            });
-        } else {
-            // If email is not verified, return 403 
-            res.status(404).json({
-                status: 404,
-                code: "not-login",
-                message: 'No user found, please log in.'
-            });
+            await auth.signOut();
         }
         
     } catch (error) {
@@ -198,21 +165,18 @@ const currentUsers = async (req, res) => {
 
 const logoutUsers = async (req, res) => {
     try {
-        // Check if user logged
-        if (auth.currentUser) {
-            await auth.signOut();
-            res.status(200).json({
-                status: 200,
-                message: 'User successfully logout'
-            });
-        } else {
-            // If not detected as authenticated user, return 404
-            res.status(404).json({
-                status: 404,
-                message: 'No account need to be logout / already logout'
-            });
+        const authHeader = req.headers.authorization;
+        const token = authHeader.split(' ')[1];
+    
+        if (token) {
+            blacklist.add(token);
         }
-        
+
+        res.status(200).json({
+            status: 200,
+            message: 'Logged out successfully' 
+        });
+
     } catch (error) {
         res.status(500).json({
             status: 500,
@@ -221,6 +185,40 @@ const logoutUsers = async (req, res) => {
         })
     }
 }
+
+const currentUsers = async (req, res) => {
+    try {
+        // Extract token from Authorization header (Bearer token)
+        const authHeader = req.headers.authorization;
+
+        const token = authHeader.split(' ')[1];
+
+        if (blacklist.has(token)) {
+            return res.status(401).json({
+                status: 401,
+                message: 'Token revoked'
+            });
+        }
+        
+        // Verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        
+        const [ data ] = await UsersModel.getCurrentUser(decoded.uid);
+        // Return user's UID
+        res.status(200).json({
+            status: 200,
+            message: "Successfuly get current user",
+            data: data
+        });
+    } catch (error) {
+        res.status(401).json({
+            status: 401,
+            message: "Unauthorized",
+            error: error.message
+        });
+    }
+}
+
 
 const forgotPasswordUsers = async (req, res) => {
     try {
