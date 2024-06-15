b5843054e7cb63e65d576cc82ed64718c0ec69dd
David <daviddwiyanto79@gmail.com>
2024-06-15 18:02:39 +0700

    feat(User Logs)

--- src/routes/openTrips.js
+++ src/routes/openTrips.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const multer = require('multer');
+const verifyToken = require('../middleware/verifyToken')
 
 const OpenTripsController = require('../controller/OpenTripsController')
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
 
+
 router.get('/', OpenTripsController.getAllOpenTrips);
 
-router.get('/:open_trip_uuid', OpenTripsController.getAllOpenTripsById);
+router.get('/:open_trip_uuid', verifyToken, OpenTripsController.getAllOpenTripsById);
 
 router.get('/partners/:partner_uid', OpenTripsController.getPartnerProfile);
 
